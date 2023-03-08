5079d888b2fc5a1a5dd55647fd3575a38e4fa349
Garret Patten <garret.patten@proton.me>
2023-03-07 23:27:10 -0500

    First commit

---
+++ src/scripts/installCliTools.sh
@@ -0,0 +1,3 @@
+# Install CLI Tools
+
+sudo dnf install bat clamav clamtk curl exa flatpak fzf git openvpn task terminator thunderbird ufw vlc zsh
---
+++ src/scripts/installFlatpakApps.sh
@@ -0,0 +1,5 @@
+# Install Flatpak Apps
+
+flatpak install flathub org.signal.Signal
+flatpak install flathub com.simplenote.Simplenote
+flatpak install flathub com.todoist.Todoist
