44d8b7cd09973cb0c0715b4e3b17990f6dcce386
Garret Patten <garret.patten@proton.me>
2023-04-30 18:15:55 -0400

    add misc script

--- src/scripts/master.sh
+++ src/scripts/master.sh
@@ -1,13 +1,15 @@
 # Begin: System Updates
 sudo dnf upgrade -y && sudo dnf update-y && sudo dnf autoremove
 
+sudo chmod +x ./
+
 # Security: YubiKeys, Firewall, VPN, Anti-Virus
 sh ./security.sh
 
 # CLI Tooling
 sh ./cli.sh
 
-# Flatpak Setup
+# Flatpak Apps
 sh ./flatpak.sh
 
 # Shell: Terminator, zsh, oh-my-zsh
@@ -21,6 +23,9 @@ sh ./web.sh
 
 # Development Setup
 sh ./dev.sh
+
+# Other
+sh ./misc.sh
 
 # Add Taskwarrior tasks
 sh ./addTasks.sh
---
+++ src/scripts/misc.sh
@@ -0,0 +1,2 @@
+# Install Mozilla Thunderbird email client
+sudo dnf install thunderbird -y
