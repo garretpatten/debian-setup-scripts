a659fdb69a1f56595a69e889eb126c6ddb4193ee
Garret Patten <garret.patten@proton.me>
2023-04-30 23:31:24 -0400

    Add VLC installation

--- src/scripts/misc.sh
+++ src/scripts/misc.sh
@@ -1,2 +1,7 @@
-# Install Mozilla Thunderbird email client
+# Thunderbird
 sudo dnf install thunderbird -y
+
+# VLC
+sudo dnf install https -y
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
+sudo dnf install vlc
