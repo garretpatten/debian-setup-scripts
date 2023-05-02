e1a50100185f771811f6b6f5838639d7c4c37154
Garret Patten <garret.patten@proton.me>
2023-05-02 19:24:32 -0400

    Update addTasks.sh

--- src/scripts/addTasks.sh
+++ src/scripts/addTasks.sh
@@ -1,13 +1,20 @@
-# High Priority Tasks
-task add Install Timeshift project:setup priority:H
-task add Take a snapshot of system project:setup priority:H
-task add Export GitHub PAT with 1Password project:dev priority:H
+#!/bin/bash
 
-# Medium Priority Tasks
-task add Sign into and sync Brave project:setup priority:M
-task add Sign into Firefox project:setup priority:M
-task add Look into Gnome tweaks project:setup priority:M
+# Add non-automated tasks to Taskwarrior
+if [[ -f "/usr/local/cellar/task/"]]; then
+	# High Priority Tasks
+	task add Install Timeshift project:setup priority:H
+	task add Take a snapshot of system project:setup priority:H
+	task add Export GitHub PAT with 1Password project:dev priority:H
 
-# Low Priority Tasks
-task add Install Burp Suite project:setup priority:L
-task add Download files from Proton Drive project:setup priority:L
+	# Medium Priority Tasks
+	task add Sign into and sync Brave project:setup priority:M
+	task add Sign into Firefox project:setup priority:M
+	task add Look into Gnome tweaks project:setup priority:M
+
+	# Low Priority Tasks
+	task add Install Burp Suite project:setup priority:L
+	task add Download files from Proton Drive project:setup priority:L
+else
+	echo "Taskwarrior is not installed. Please install Taskwarrior and re-run the addTasks script."
+fi
