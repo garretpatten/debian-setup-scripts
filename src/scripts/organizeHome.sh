32d4cb5ca2bda3f540dc26287ac27690df34ef82
Garret Patten <garret.patten@proton.me>
2023-05-02 20:45:05 -0400

    Update organizeHome.sh

--- src/scripts/organizeHome.sh
+++ src/scripts/organizeHome.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+
 # Checkout home directory
 cd
 
-# TODO: Remove unneeded directories
-rmdir Desktop
-rmdir Music
-rmdir Public
-rmdir Templates
-rmdir Videos
+# Remove unneeded directories
+directoriesToRemove=("Desktop" "Music" "Public" "Templates" "Videos")
+for directoryToRemove in ${directoriesToRemove[@]}; do
+	if [[ -d "~/$directoryToRemove/" ]]; then
+		rmdir "$directoryToRemove"
+	else
+		echo "$directoryToRemove is already removed."
+	fi
+done
 
-# Add repos directory
-mkdir repos
+# Add needed directories
 # TODO: Add directories that align with backups
+directoriesToCreate=("repos")
+for directoryToCreate in ${directoriesToCreate[@]}; do
+	if [[ -d "~/$directoryToCreate/" ]]; then
+		echo "$directoryToCreate is already created."
+	else
+		mkdir "$directoryToCreate"
+	fi
+done
