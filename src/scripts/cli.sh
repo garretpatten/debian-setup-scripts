eaf280ece48caf9a3bf9053f727aaa03264d2b8a
Garret Patten <garret.patten@proton.me>
2023-05-02 21:01:55 -0400

    Update cli.sh

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,7 +1,7 @@
 # Install basic CLI tools
 cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "wget")
 for tool in ${cliTools[@]}; do
-	if [[ -d "/usr/local/cellar/$tool/" ]]; then
+	if [[ -d "/usr/local/bin/$tool/" ]]; then
 			echo "$tool is already installed."
 	else
 		sudo dnf install "$tool" -y
