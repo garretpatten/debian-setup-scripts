329dd17755217871481be78a145f5a2131348846
Garret Patten <garret.patten@proton.me>
2023-05-02 20:37:24 -0400

    Update flatpak.sh

--- src/scripts/flatpak.sh
+++ src/scripts/flatpak.sh
@@ -1,16 +1,22 @@
 # Install Flatpak
-sudo dnf install flatpak -y
+if [[ -f "/usr/local/bin/flatpak" ]]; then
+	echo "flatpak is already installed."
+else
+	sudo dnf install flatpak -y
+fi
 
 # Add remote Flatpak repos
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # TODO: Add FlatHub remote
 
-# Install Signal Messenger
-flatpak install flathub org.signal.Signal -y
-
-# Install Simplenote
-flatpak install flathub com.simplenote.Simplenote -y
-
-# Install Spotify
-flatpak install flathub com.spotify.Client -y
+# Install Signal Messenger, Simplenote, Spotify
+flatpakApps=("org.signal.Signal" "com.simplenote.Simplenote" "com.spotify.Client")
+for flatpakApp in ${flatpakApps[@]}; do
+	# TODO: Path to flatpak apps is either /var/lib/flatpak or ~/.local/share/flatpak
+	if [[ -d "path/to/$flatpakApp" ]]; then
+		echo "$flatpak is already installed."
+	else
+		flatpak install flathub "$flatpak" -y
+	fi
+done
