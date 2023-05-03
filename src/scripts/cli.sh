587f9cdb9a8a2f7641fc4884ef8422cd4c3d4e4d
Garret Patten <garret.patten@proton.me>
2023-05-02 21:01:28 -0400

    Update cli.sh

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,2 +1,9 @@
-# Install CLI Tools
-sudo dnf install bat curl exa fzf git https htop openvpn wget -y
+# Install basic CLI tools
+cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "wget")
+for tool in ${cliTools[@]}; do
+	if [[ -d "/usr/local/cellar/$tool/" ]]; then
+			echo "$tool is already installed."
+	else
+		sudo dnf install "$tool" -y
+	fi
+done
