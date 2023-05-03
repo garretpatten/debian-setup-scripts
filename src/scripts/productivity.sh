5235af718b0fba05b171e99ed699262abb3d996f
Garret Patten <garret.patten@proton.me>
2023-05-02 20:50:53 -0400

    Update productivity.sh

--- src/scripts/productivity.sh
+++ src/scripts/productivity.sh
@@ -1,17 +1,29 @@
 # Install taskwarrior
 sudo dnf install task -y
 
-# Update config file
+# Taskwarrior config
 cat "$(pwd)/src/artifacts/taskwarrior/taskrcUpdates.txt" >> ~/.taskrc
 
-# TODO: Run python script to set dark blue theme
-
 # Add directory for custom themes
-mkdir ~/.task/themes/
+if [[ -d "~/.task/themes/" ]]; then
+	echo "Taskwarrior themes directory already exists."
+else
+	mkdir ~/.task/themes/
+fi
 
 ### TODO: Update this from copying an artifact to pulling themes from GitHub ###
 # Add custom themes to directory
 cp "$(pwd)/src/artifacts/taskwarrior/themes/" ~/.task/themes/
 
-# Install Todoist
-flatpak install flathub com.todoist.Todoist -y
+# TODO: Set dark blue theme
+
+# Install Simplenote and Todoist
+flatpakApps=("com.simplenote.Simplenote" "com.todoist.Todoist")
+for flatpakApp in ${flatpakApps[@]}; do
+	# TODO: Path to flatpak apps is either /var/lib/flatpak or ~/.local/share/flatpak
+	if [[ -d "path/to/$flatpakApp" ]]; then
+		echo "$flatpakApp is already installed."
+	else
+		brew install --cask "$flatpakApp"
+	fi
+done
