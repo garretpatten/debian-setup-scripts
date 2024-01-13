9ff8c9a194136c70428f5397b02d7957d811cbf2
Garret Patten <garret.patten@proton.me>
2024-01-12 22:18:44 -0500

    Fix cli.sh

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -4,14 +4,14 @@ packageManager=$1
 
 # Foundational CLI tools
 cliTools=("bat" "curl" "exa" "git" "htop" "neofetch" "openvpn" "vim" "wget")
-for cliTool in ${cliTools[@]}; do
+for cliTool in "${cliTools[@]}"; do
 	if [[ -d "/usr/bin/$cliTool" ]]; then
 		echo "$cliTool is already installed."
 	elif [[ -f "/usr/sbin/$cliTool" ]]; then
 		echo "$cliTool is already installed."
 	else
 		if [[ "$packageManager" = "apt" || "$packageManager" = "dnf" ]]; then
-			sudo $packageManager install "$cliTool" -y
+			sudo "$packageManager" install "$cliTool" -y
 		elif [[ "$packageManager" = "pacman" ]]; then
 			sudo pacman -S --noconfirm "$cliTool"
 		else
@@ -28,7 +28,7 @@ else
 		sudo apt install python3.6 -y
 	elif [[ "$packageManager" = "dnf" ]]; then
 		sudo dnf install python3 -y
-	elif [[ "$packageManaer" = "pacman" ]]; then
+	elif [[ "$packageManager" = "pacman" ]]; then
 		sudo pacman -S --noconfirm python3
 	else
 		echo "Support has only been added for apt, dnf, and pacman."
