4913b930c59ff940763e96440911d759a565e072
Garret Patten <garret.patten@proton.me>
2024-07-04 23:58:15 -0400

    Remove params and more pacman stuff

--- src/scripts/master.sh
+++ src/scripts/master.sh
@@ -2,78 +2,47 @@
 
 # TODO: cd to the root of the project.
 
-errorMessage=" is not supported for the $packageManager package manager."
-packageManager=""
 workingDirectory=$(pwd)
 
 if [[ -f "/usr/bin/apt-get" ]]; then
     packageManager="apt-get"
 elif [[ -f "/usr/bin/dnf" ]]; then
     packageManager="dnf"
-elif [[ -f "/usr/bin/pacman" ]]; then
-    packageManager="pacman"
-    if [[ ! -f "/usr/bin/yay" ]]; then
-        sudo pacman -S --noconfirm base-devel
-        sudo pacman -S --noconfirm git
-
-        cd ~/Downloads || return
-        git clone https://aur.archlinux.org/yay.git
-        cd yay || return
-        makepkg -sri --noconfirm
-
-        cd "$workingDirectory" || return
-    fi
-fi
-
-# Exit if package manager is not supported.
-if [[ "$packageManager" = "" ]]; then
-    echo "The package manager on this system is not supported."
-    echo "The following package managers are supported:"
-    echo "apt-get, dnf, pacman"
-    exit 1
 fi
 
 # Update the system.
-if [[ "$packageManager" = "pacman" ]]; then
-    sudo pacman -Syu --noconfirm && yay -Yc --noconfirm
-else
-    sudo "$packageManager" update -y && sudo "$packageManager" upgrade -y && sudo "$packageManager" autoremove -y
-fi
+sudo "$packageManager" update -y && sudo "$packageManager" upgrade -y && sudo "$packageManager" autoremove -y
 
 # Organize Directories.
 sh "$workingDirectory/src/scripts/organizeHome.sh"
 
 # CLI Tooling.
-sh "$workingDirectory/src/scripts/cli.sh" "$errorMessage" "$packageManager"
+sh "$workingDirectory/src/scripts/cli.sh"
 
 # Security: YubiKeys, Firewall, VPN, Anti-Virus.
-bash "$workingDirectory/src/scripts/security.sh" "$errorMessage" "$packageManager" "$workingDirectory"
+bash "$workingDirectory/src/scripts/security.sh" "$workingDirectory"
 
 # Productivity: Notion, Simplenote, Taskwarrior, Todoist.
-bash "$workingDirectory/src/scripts/productivity.sh" "$errorMessage" "$packageManager" "$workingDirectory"
+bash "$workingDirectory/src/scripts/productivity.sh" "$workingDirectory"
 
 # Web Apps.
-bash "$workingDirectory/src/scripts/web.sh" "$errorMessage" "$packageManager"
+bash "$workingDirectory/src/scripts/web.sh"
 
 # Development Setup.
-bash "$workingDirectory/src/scripts/dev.sh" "$errorMessage" "$packageManager" "$workingDirectory"
+bash "$workingDirectory/src/scripts/dev.sh" "$workingDirectory"
 
 # Shell: Terminator, zsh, oh-my-zsh.
-zsh "$workingDirectory/src/scripts/shell.sh" "$errorMessage" "$packageManager" "$workingDirectory"
+zsh "$workingDirectory/src/scripts/shell.sh" "$workingDirectory"
 
 # Hacking: Burp Suite, Black Arch/Kali tools.
-bash "$workingDirectory/src/scripts/hacking.sh" "$errorMessage" "$packageManager" "$workingDirectory"
+bash "$workingDirectory/src/scripts/hacking.sh" "$workingDirectory"
 
 # Other: Signal, Spotify, Thunderbird.
-bash "$workingDirectory/src/scripts/misc.sh" "$errorMessage" "$packageManager"
+bash "$workingDirectory/src/scripts/misc.sh"
 
 # Update the system.
 if [[ "$packageManager" = "apt-get" || "$packageManager" = "dnf" ]]; then
     sudo "$packageManager" update -y && sudo "$packageManager" upgrade -y && flatpak update -y && sudo "$packageManager" autoremove -y
-elif [[ "$packageManager" = "pacman" ]]; then
-    sudo pacman -Syu --noconfirm && yay -Yc --noconfirm
-else
-    echo "Error Message"
 fi
 
 printf "\n\n============================================================================\n\n"
