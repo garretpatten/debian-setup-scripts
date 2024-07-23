e2f6f4983f3496eb86811d200eae61f1b4c232ad
Garret Patten <garret.patten@proton.me>
2024-07-22 21:25:18 -0400

    Update dev and ide scripts

--- src/scripts/dev.sh
+++ src/scripts/dev.sh
@@ -15,24 +15,7 @@ fi
 
 ### Runtimes ###
 
-
-
-# Neovim config
-cp "$workingDirectory/src/dotfiles/nvim/init.vim" ~/.config/nvim/init.vim
-
-# Vim config
-cd "$workingDirectory" || return
-cd src/dotfiles/vim/
-cp .vimrc ~/.vimrc
-cd .. || return
-cd nvim
-mkdir -p ~/.config/nvim/
-cp init.vim ~/.config/nvim/init.vim
-cd "$workingDirectory" || return
-
-### Runtimes ###
-
-# Node & npm
+# Node.js & npm
 if [[ "$packageManager" = "apt-get" ]]; then
     # nosemgrep: bash.curl.security.curl-pipe-bash.curl-pipe-bash Installation comes from Debian docs
     curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
@@ -84,70 +67,10 @@ if [[ ! -f "$HOME/.local/bin/semgrep" ]]; then
 fi
 
 # Shellcheck
-if [[ "$packageManager" = "apt-get" ]]; then
-    sudo apt install shellcheck -y
-fi
+sudo apt install shellcheck -y
 
 # Sourcegraph
 if [[ ! -f "/usr/local/bin/src" ]]; then
     curl -L https://sourcegraph.com/.api/src-cli/src_linux_amd64 -o "/usr/local/bin/src"
     chmod +x "/usr/local/bin/src"
 fi
-
-# VS Code
-if [[ ! -f "/usr/bin/code" ]]; then
-    isInstalled="true"
-    if [[ "$packageManager" = "apt-get" ]]; then
-        cd ~/Downloads || return
-
-        wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-
-        cd "$workingDirectory" || return
-
-        sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-        sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-        sudo apt-get install apt-transport-https -y
-        sudo apt-get update -y
-        sudo apt-get install code -y
-    elif [[ "$packageManager" = "dnf" ]]; then
-        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-        sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-        sudo dnf check-update -y
-        sudo dnf install code -y
-    elif [[ "$packageManager" = "pacman" ]]; then
-        sudo pacman -S --noconfirm code
-    else
-        isInstalled="false"
-        echo "VS Code $errorMessage"
-    fi
-
-    if [[ "$isInstalled" = "true" ]]; then
-        cp "$workingDirectory/src/config-files/vs-code/settings.json" ~/.config/'Code - OSS'/User/settings.json
-    fi
-fi
-
-### Fonts ###
-
-# Fira Code
-if [[ -d "/usr/share/fonts/FiraCode/" ]]; then
-    echo "Fira Code is already installed."
-    if [[ "$packageManager" = "pacman" ]]; then
-        cd ~/Downloads || return
-
-        git clone https://aur.archlinux.org/ttf-firacode.git
-        cd ttf-firacode || return
-        makepkg -sri --noconfirm
-
-        cd "$workingDirectory" || return
-    else
-        echo "Fira Code $errorMessage"
-    fi
-fi
-
-### Package Managers ###
-
-# Packer installation
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
--- src/scripts/ide.sh
+++ src/scripts/ide.sh
@@ -15,3 +15,20 @@ if [[ ! -d "$HOME/.config/nvim/" ]]; then
     "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
     sudo apt install tree-sitter-cli -y
 fi
+
+# VS Code
+if [[ ! -f "/usr/bin/code" ]]; then
+    cd "$HOME/Downloads" || return
+
+    wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+    cd "$workingDirectory" || return
+
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+    sudo apt-get install apt-transport-https -y
+    sudo apt-get update -y
+    sudo apt-get install code -y
+fi
