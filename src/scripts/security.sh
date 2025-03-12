dec6c1dee4b216d48b74f4e5d74972ac06fcb239
Garret Patten <garret.patten@proton.me>
2025-03-12 13:00:29 -0400

    feat: Add Proton VPN installation

--- src/scripts/security.sh
+++ src/scripts/security.sh
@@ -46,7 +46,11 @@ sudo ufw enable
 
 # Proton VPN, Proton VPN CLI, and system tray icon
 if [[ ! -f "/usr/bin/protonvpn" ]]; then
-    # TODO: Install Proton VPN client and CLI
+    wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
+    sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
+    echo "0b14e71586b22e498eb20926c48c7b434b751149b1f2af9902ef1cfe6b03e180 protonvpn-stable-release_1.0.8_all.deb" | sha256sum --check -
+    sudo apt install proton-vpn-gnome-desktop -y
+    sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
 fi
 
 # Signal Messenger
